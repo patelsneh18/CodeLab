2c912894a836e0e7c892dbb031b5eda3299dd33e
Sneh Patel <sneh.19bcon321@gmail.com>
2021-05-02 00:37:28 +0530

    Added Comments

--- app/src/main/java/com/google/codelab/MainActivity.java
+++ app/src/main/java/com/google/codelab/MainActivity.java
@@ -17,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Shows toast saying Hello Toast!
+     * @param view root of ui
+     */
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message,
                 Toast.LENGTH_SHORT);
         toast.show();
     }
 
+    /**
+     * Increases the count shown in text fied by 1
+     * @param view root of ui
+     */
     public void countUp(View view) {
         ++mCount;
         TextView mShowCount = (TextView) findViewById(R.id.show_count);
