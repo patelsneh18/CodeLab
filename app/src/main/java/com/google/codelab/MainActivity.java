2904ac7928484fa151f0707fd6f9855ed79299f4
Sneh Patel <sneh.19bcon321@gmail.com>
2021-05-02 12:38:05 +0530

    .

--- app/src/main/java/com/google/codelab/MainActivity.java
+++ app/src/main/java/com/google/codelab/MainActivity.java
@@ -9,24 +9,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mCount = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void countUp(View view) {
-        ++mCount;
-        TextView mShowCount = (TextView) findViewById(R.id.show_count);
-        if (mShowCount != null)
-            mShowCount.setText("" + mCount);
-    }
 }
